c519bdcf6d8eda0384c539ee22e4fc35dd825b45
psnipiv <psnipiv@.github.com>
2018-01-15 00:05:05 +0800

    Modification20180115
    Update to redirect to login screen.

--- LicenseGenerator/CustomerInformation/views.py
+++ LicenseGenerator/CustomerInformation/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     companies = Company.objects.all()
     return render(request, 'home.html', {'companies': companies})
